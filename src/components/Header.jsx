669ac5e3bad4ef9d1c0f6f3ba41c0236a417e1f4
Birajit Saikia <birajitsaikia@gmail.com>
2024-12-29 00:17:20 +0530

    made whole header changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -70,11 +70,11 @@ const Header = () => {
 
         <a
           href="#signup"
-          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block nav-item-font"
         >
           New account
         </a>
-        <Button className="hidden lg:flex" href="#login">
+        <Button className="hidden lg:flex nav-item-font" href="#login">
           Sign in
         </Button>
 
