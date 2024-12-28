7817be0a5c16aeec5ea126fb84ee936d3f2775b2
Birajit Saikia <birajitsaikia@gmail.com>
2024-12-29 00:14:34 +0530

    gave preferences to the font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
-import './Header.css'; // Import the CSS file
+import './Header.css';
 
 const Header = () => {
   const pathname = useLocation();
