9f5841cf7569d26f6c5bce2adb4b24f3b5f92dc8
Birajit Saikia <birajitsaikia@gmail.com>
2025-01-01 04:48:30 +0530

    new pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import App from "./App.jsx";
+import SignIn from "./components/SignIn.jsx";
+import NewAccount from "./components/NewAccount.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/newaccount" element={<NewAccount />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
